8be22844e172c8b2777d8ba0b45f57fa547518bc
Saman Khan <147999773+Samankhan7860@users.noreply.github.com>
2024-09-08 01:41:55 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 var _a;
 // Listing element
 (_a = document.getElementById('resumeForm')) === null || _a === void 0 ? void 0 : _a.addEventListener('submit', function (event) {
+    var _a;
     event.preventDefault();
     //type assertion
+    var profilePictureInput = document.getElementById('profilePicture');
     var nameElement = document.getElementById('name');
     var emailElement = document.getElementById('email');
     var phoneElement = document.getElementById('phone');
+    var addressElement = document.getElementById('address');
     var educationElement = document.getElementById('education');
     var experienceElement = document.getElementById('experience');
     var skillsElement = document.getElementById('skills');
-    if (nameElement && emailElement && phoneElement && educationElement && experienceElement && skillsElement) {
+    if (profilePictureInput && nameElement && emailElement && phoneElement && addressElement && educationElement && experienceElement && skillsElement) {
         var name_1 = nameElement.value;
         var email = emailElement.value;
         var phone = phoneElement.value;
+        var address = addressElement.value;
         var education = educationElement.value;
         var experience = experienceElement.value;
         var skills = skillsElement.value;
+        // create picture element
+        var profilePicture = (_a = profilePictureInput.files) === null || _a === void 0 ? void 0 : _a[0];
+        var profilePictureURL = profilePicture ? URL.createObjectURL(profilePicture) : "";
         //create resume output 
-        var resumeOutput = "\n    <h1>Resume</h1>\n    <p><strong>Name:</strong><span id=\"edit-name\" class=\"editable\"> ".concat(name_1, " </span> </p>\n    <p><strong>Email:</strong><span id=\"edit-email\" class=\"editable\"> ").concat(email, " </span> </p>\n    <p><strong>Phone: </strong><span id=\"edit-phone\" class=\"editable\"> ").concat(phone, " </span> </p>\n  \n    <h3>Education</h3>\n    <p id=\"edit-education\" class=\"editable\">").concat(education, "</p>\n\n\n    <h3>Experience</h3>\n    <p id=\"edit-experience\" class=\"editable\">").concat(experience, "</p>\n\n    <h3>Skills</h3>\n    <p id=\"edit-skills\" class=\"editable\">").concat(skills, "</p>\n    ");
+        var resumeOutput = "\n    <h1>Resume</h1>\n    ".concat(profilePicture ? "<img src=\"".concat(profilePictureURL, "\" alt=\"Profile Picture\" class=\"profilePicture\">") : '', "\n    <p><strong>Name:</strong><span id=\"edit-name\" class=\"editable\"> ").concat(name_1, " </span> </p>\n    <p><strong>Email:</strong><span id=\"edit-email\" class=\"editable\"> ").concat(email, " </span> </p>\n    <p><strong>Phone: </strong><span id=\"edit-phone\" class=\"editable\"> ").concat(phone, " </span> </p>\n    <p><strong>Address: </strong><span id=\"edit-address\" class=\"editable\"> ").concat(address, " </span> </p>\n  \n    <h3>Education</h3>\n    <p id=\"edit-education\" class=\"editable\">").concat(education, "</p>\n\n\n    <h3>Experience</h3>\n    <p id=\"edit-experience\" class=\"editable\">").concat(experience, "</p>\n\n    <h3>Skills</h3>\n    <p id=\"edit-skills\" class=\"editable\">").concat(skills, "</p>\n    ");
         //add resume output to resume output element
         var resumeOutputElement = document.getElementById('resumeOutput');
         if (resumeOutputElement) {
